3c135dec7ebd27f87daeca52685cfcb24cb42196
Like-all <like-all@yandex.com>
2015-11-06 23:31:51 +0200

    leicht_send_message_to_channel function

--- src/lib/bash/leicht.sh
+++ src/lib/bash/leicht.sh
@@ -7,3 +7,14 @@ function leicht_send_message() {
 
     echo '{ "actionType": "SendMessage", "actionSettings": {"chatID": '$chat_id', "replyToMessageID": '$reply_id', "text": '$message_text', "disableWebPagePreview": '$disable_web_preview'}}' | socat stdio unix-connect:$5
 }
+
+function leicht_send_message_to_channel() {
+    channel_username=$1
+    reply_id=$2
+    message_text=`echo "$3" | jq -R -s '.'`
+    disable_web_preview=$4
+    leicht_socket=$5
+
+    echo '{ "actionType": "SendMessage", "actionSettings": {"channelUsername": "'$channel_username'", "replyToMessageID": '$reply_id', "text": '$message_text', "disableWebPagePreview": '$disable_web_preview'}}' | socat stdio unix-connect:$5
+}
+
